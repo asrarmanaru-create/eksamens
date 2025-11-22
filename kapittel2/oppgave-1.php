632fa5f7dbdc8b282cb8741165a3e427f5fe95af
asrarmanaru-create <asrarmanaru@gmail.com>
2025-11-22 09:39:03 +0100

    as

---
+++ kapittel2/oppgave-1.php
@@ -0,0 +1,15 @@
+<?php 
+$svar = $_POST["svar"]; 
+$fasisvar = "9"; 
+
+if ($svar== $fasisvar) 
+{
+    print ("riktig. 3 ganger 3 er $svar"); 
+
+}
+else 
+    {
+    print ("feil. 3 ganger 3 er ikke $svar");
+    }
+
+?>
