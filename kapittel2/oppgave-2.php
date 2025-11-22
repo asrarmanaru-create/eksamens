6859fde72132e7bccf7610d17a774211942e11e5
asrarmanaru-create <asrarmanaru@gmail.com>
2025-11-22 13:00:51 +0100

    asra

--- kapittel2/oppgave-2.php
+++ kapittel2/oppgave-2.php
@@ -1,17 +1,20 @@
 <?php
-$Svar = $_POST["svar"];
+/* Oppgave 2 */
+/*
+/* Programmet mottar fra et HTML-skjema et svar på spørsmålet "Er du student (j/n) ?"
+/* Programmet sjekker om det er svart j eller n på spørsmålet og skriver ut en passende melding
+*/
 
-if (!$Svar) {
-    print("Du har ikke svart på spørsmålet ennå");
-}
-else if ($Svar == "j") {
-    print("Du har svart ja på spørsmålet, og du er student");
-}
-else if ($Svar == "n") {
-    print("Du har svart nei på spørsmålet, og du er ikke student");
-}
-else {
-    print("Du har ikke svart ja eller nei på spørsmålet");
+if (!isset($_POST["svar"])) {
+    print("Du har ikke svart p&aring; sp&oslash;rsm&aring;let om du er student <br/>");
+} else {
+    $svar = $_POST["svar"];
+    if ($svar == "j") {
+        print("Du har svart ja p&aring; sp&oslash;rsm&aring;let om du er student <br/>");
+    } else if ($svar == "n") {
+        print("Du har svart nei p&aring; sp&oslash;rsm&aring;let om du er student <br/>");
+    } else {
+        print("Du har ikke svart ja eller nei p&aring; sp&oslash;rsm&aring;let om du er student <br/>");
+    }
 }
 ?>
-    
