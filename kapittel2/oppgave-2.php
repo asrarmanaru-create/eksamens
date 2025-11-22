301131209ed3b9d2b75a93e413b9fb7a95b6eb40
asrarmanaru-create <asrarmanaru@gmail.com>
2025-11-22 13:03:09 +0100

    gfg

--- kapittel2/oppgave-2.php
+++ kapittel2/oppgave-2.php
@@ -1,10 +1,4 @@
 <?php
-/* Oppgave 2 */
-/*
-/* Programmet mottar fra et HTML-skjema et svar på spørsmålet "Er du student (j/n) ?"
-/* Programmet sjekker om det er svart j eller n på spørsmålet og skriver ut en passende melding
-*/
-
 if (!isset($_POST["svar"])) {
     print("Du har ikke svart p&aring; sp&oslash;rsm&aring;let om du er student <br/>");
 } else {
