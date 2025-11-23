9ff6dd8be37d92a3ac8b77c99e7207980b231834
asrarmanaru-create <asrarmanaru@gmail.com>
2025-11-23 17:57:43 +0100

    asas

---
+++ kapittel3/oppgave-3.php
@@ -0,0 +1,21 @@
+<?php 
+// første linje: 1 til 10
+for ($tall = 1; $tall <= 10; $tall++) {
+    print "$tall" ; 
+
+}
+print "<br>"; 
+
+// andre linje: 11 til 20 
+for ($tall = 11; $tall <= 20; $tall++) { 
+    print "$tall "; 
+
+}
+print "<br>"; 
+// tredje linje: 21 til 30 
+for ($tall = 21; $tall <= 30; $tall++) {
+    print "$tall "; 
+
+}
+print "<br>"; 
+?>
