0efba41adc925ed07f601235bcc7dc3e9fed4afa
asrarmanaru-create <asrarmanaru@gmail.com>
2025-11-23 13:26:06 +0100

    asrar

--- kapittel2/oppgave-7.php
+++ kapittel2/oppgave-7.php
@@ -4,38 +4,34 @@ $tall1 = $_POST["tall1"];
 $tall2 = $_POST["tall2"];
 $tall3 = $_POST["tall3"];
 
-print(" tall 1 er $tall1 <br/>");
-print(" tall 2 er $tall2 <br/>");
-print(" tall 3 er $tall3 <br/>");
+print("Tall 1 er $tall1 <br/>");
+print("Tall 2 er $tall2 <br/>");
+print("Tall 3 er $tall3 <br/><br/>");
 
+// Sammenligning: tall 1 og tall 2
 if ($tall1 < $tall2) {
-    print(" tall 1 er mindre enn tall 2<br/>");
-}
-else if ($tall1 == $tall2) {
-    print(" tall 1 er lik tall 2");
-}
-else {
-    print(" tall 1 er st&oslash;rre enn tall 2<br/>");
+    print("Tall 1 er mindre enn Tall 2<br/>");
+} elseif ($tall1 == $tall2) {
+    print("Tall 1 er lik Tall 2<br/>");
+} else {
+    print("Tall 1 er større enn Tall 2<br/>");
 }
 
+// Sammenligning: tall 1 og tall 3
 if ($tall1 < $tall3) {
-    print(" tall 1 er mindre enn tal 3<br/>");
-}
-else if ($tall1 == $tall3) {
-    print(" tall 1 er like tall 3<br/>");
-}
-else {
-    print(" tall 1 er st&oslash;rre enn tall 3<br/>");
+    print("Tall 1 er mindre enn Tall 3<br/>");
+} elseif ($tall1 == $tall3) {
+    print("Tall 1 er lik Tall 3<br/>");
+} else {
+    print("Tall 1 er større enn Tall 3<br/>");
 }
 
-
+// Sammenligning: tall 2 og tall 3
 if ($tall2 < $tall3) {
-    print(" tall 2 er mindre enn tall 3<br/>");
-}
-else if ($tall2 == $tall3) {
-    print (" tall 2 er lik tall 3<br/>");
-}
-else {
-    print (" tall 2 er st&oslash;rre enn tall 3<br/>");
+    print("Tall 2 er mindre enn Tall 3<br/>");
+} elseif ($tall2 == $tall3) {
+    print("Tall 2 er lik Tall 3<br/>");
+} else {
+    print("Tall 2 er større enn Tall 3<br/>");
 }
 ?>
